d91488f15d9f833efeb81fb1ff5e1a7418afe283
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2021-06-28 18:58:55 +0700

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,6 +1,11 @@
 <?php 
 
-$conn = mysqli_connect("ec2-35-168-145-180.compute-1.amazonaws.com", "vqitiqwilsqeqi", "5432", "d5d9ao479npn9u");
+$host = 'ec2-35-168-145-180.compute-1.amazonaws.com';
+$dbname = 'd5d9ao479npn9u';
+$username = 'vqitiqwilsqeqi';
+$password = '44fc7f13b3ee737fdacfc088b1238e8f825896170cd03535044ef6b2e9909358';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 if(!$conn){
 	echo "error";
