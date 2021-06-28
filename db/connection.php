ac726431e8e6925618160617f21c340490315f99
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2021-06-28 18:54:10 +0700

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "", "psycholight");
+$conn = mysqli_connect("ec2-35-168-145-180.compute-1.amazonaws.com", "vqitiqwilsqeqi", "5432", "d5d9ao479npn9u");
 
 if(!$conn){
 	echo "error";
