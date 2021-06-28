a702959bead6036e06f0ba0c7baeb50529c19544
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2021-06-28 19:01:09 +0700

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,11 +1,6 @@
 <?php 
 
-$host = 'ec2-35-168-145-180.compute-1.amazonaws.com';
-$dbname = 'd5d9ao479npn9u';
-$username = 'vqitiqwilsqeqi';
-$password = '44fc7f13b3ee737fdacfc088b1238e8f825896170cd03535044ef6b2e9909358';
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$conn = mysqli_connect("localhost", "root", "", "psycholight");
 
 if(!$conn){
 	echo "error";
