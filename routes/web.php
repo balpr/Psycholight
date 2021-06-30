bfec8b77f31b366a8132a0b8eb5fb92ab407217b
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2021-06-30 19:31:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['scheme' => 'https'], function () {
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('user.index');
-    Route::get('blog', [DashboardController::class, 'show'])->name('user.blog');
-    Route::get('admin', [DashboardController::class, 'index'])->name('admin.index');
-    Route::get('admin/profile', function () {
-        return view('admin.profile');
-    })->name('admin.profile');
-});
+Route::get('/', function () {
+    return view('welcome');
+})->name('user.index');
+Route::get('blog', [DashboardController::class, 'show'])->name('user.blog');
+Route::get('admin', [DashboardController::class, 'index'])->name('admin.index');
+Route::get('admin/profile', function () {
+    return view('admin.profile');
+})->name('admin.profile');
 Route::resource('admin/authors', AuthorController::class);
 Route::resource('admin/blogs', BlogController::class);
 Route::resource('admin/categories', CategoryController::class);
